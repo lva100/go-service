d358dc1798f554d49c080d85b7836ca55f0dd78c
lva100 <lva100@yandex.ru>
2025-10-22 12:32:40 +0300

    Add Chi router and CRON library

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
+)
+
+const (
+	PORT = ":3000"
+)
 
 func main() {
-	fmt.Println("Service starting...")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("welcome"))
+	})
+	fmt.Printf("Service starting on port %s\n", PORT)
+	// http.ListenAndServe(PORT, r)
+
+	// create a scheduler
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		log.Fatalf("Error %s", err)
+	}
+
+	// add a job to the scheduler
+	j, err := s.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("Job working...")
+			},
+		),
+	)
+	if err != nil {
+		// handle error
+	}
+	// each job has a unique id
+	fmt.Println(j.ID())
+
+	// start the scheduler
+	s.Start()
+	time.Sleep(60 * time.Second)
+
+	// when you're done, shut it down
+	// err = s.Shutdown()
+	// if err != nil {
+	// 	// handle error
+	// }
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/lva100/go-service
 
-go 1.23.6
+go 1.24.0
+
+toolchain go1.24.9
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/go-co-op/gocron/v2 v2.17.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jonboulle/clockwork v0.5.0 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+)
